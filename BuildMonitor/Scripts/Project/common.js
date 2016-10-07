314ae289fe831e4a9acb41cf5452dea677463b2d
Gyorgy Balassy <Gyorgy.Balassy@logmein.com>
2016-10-07 13:45:28 +0200

    ADDED: Dummy data is returned from the server.

---
+++ BuildMonitor/Scripts/Project/common.js
@@ -0,0 +1,5 @@
+﻿function displayAjaxError(xhr, textStatus, errorThrown) {
+	$("#last-updated")
+		.text("Ajax error: " + textStatus + ". Details: " + errorThrown + " (HTTP " + xhr.status + ").")
+		.addClass("update-failure");
+}
